346b4cee3d528442727cccaf6c51677cbcc44ce2
Myr-camila-Mg <sunmin18@outlook.com>
2024-07-06 14:54:00 -0500

    tabla en array

--- front/free-style/src/pages/Precios.js
+++ front/free-style/src/pages/Precios.js
@@ -2,31 +2,38 @@ import React from "react";
 import "../styles/Precios.css";
 
 const Precios = () => {
+    const pre = [{
+        "tipo": "regular",
+        "horario": "6:00 am - 10:00 am / 4:00 pm - 11:00 pm",
+        "precio" : "80000"
+    },
+    {
+        "tipo": "no regular",
+        "horario": "10:00 am - 4:00 am",
+        "precio" : "5000"
+    }];
     return (
-    <div className="tabla-container">
+        <div className="tabla-container">
         <table className="tabla-precio">
-        <thead className="cabezapre">
-            <tr>
-                <th></th>
+            <thead className="cabezapre">
+                    <tr>
+                <th></th> 
                 <th>Horario</th>
                 <th>Precio</th>
             </tr>
-        </thead>
-        <tbody>
-            <tr className="azul">
-                <td>Regular</td>
-                <td>6:00 am - 10:00 am / 4:00 pm - 11:00 pm</td>
-                <td>$80.000 (Precio grupal)</td>
-            </tr>
-            <tr className="amarillo">
-                <td>No regular</td>
-                <td>10:00 am - 4:00 am</td>
-                <td>$5.000 (Precio individual)</td>
-            </tr>
-        </tbody>
-    </table>
-    </div>
+            </thead>
+            <tbody className="azul">
+            {pre.map((pre, index) => (
+            <tr key={index}>
+                <td>{pre.tipo}</td>
+                <td>{pre.horario}</td>
+                <td>{pre.precio}</td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+        </div>
     );
-};
+    };
 
 export default Precios;
